020a491398213b6364e693213e1ee9f13e75ea49
dieinzige <dieinzige@me.com>
2010-08-23 19:26:21 +0400

    deprecation warning for request

--- lib/traits/controller/authorizations/authlogic.rb
+++ lib/traits/controller/authorizations/authlogic.rb
@@ -65,7 +65,7 @@ module Traits::Controller::Authorization::Authlogic
          end
 
          def store_location
-           session[:return_to] = request.request_uri
+           session[:return_to] = request.fullpath
          end
 
          def redirect_back_or_default(default)
