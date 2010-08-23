5a3e85b484a34595c915ee80786e52486f756e9f
dieinzige <dieinzige@me.com>
2010-08-23 16:35:52 +0400

    resource full

--- lib/traits/controller/resource.rb
+++ lib/traits/controller/resource.rb
@@ -1,7 +1,36 @@
 module Traits
   module Controller
     module Resource
+      module ClassMethods
+        def self.extended(reciever)
+          reciever.before_filter "find_#{reciever.controller_name.singularize}"
+        end
+        
+        def singular_name
+          controller_name.singularize
+        end
+      end
       
+      module InstanceMethods
+        def self.included(receiver)
+          receiver.send(:define_method, "find_#{receiver.singular_name}") do
+            instance_variable_set("@#{singular_name}", resource_class.find(params[:id])) if params[:id]
+          end
+        end
+        
+        def singular_name
+           controller_name.singularize
+         end
+        
+        def resource_class
+          singular_name.classify.constantize
+        end
+      end
+      
+      def self.included(receiver)
+        receiver.extend         ClassMethods
+        receiver.send :include, InstanceMethods
+      end
     end
   end
 end
