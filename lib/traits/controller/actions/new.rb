1e3d935f525d830a07365ca38bc94ad737bb4c2d
dieinzige <dieinzige@me.com>
2010-09-04 14:34:01 +0400

    new and edit implement

--- lib/traits/controller/actions/new.rb
+++ lib/traits/controller/actions/new.rb
@@ -1,7 +1,7 @@
-module Traits::Controller::Actions::Edit
+module Traits::Controller::Actions::New
   module InstanceMethods
-    def edit
-      resource = instance_variable_get("@#{singular_name}")
+    def new
+      resource = instance_variable_set("@#{singular_name}", resource_class.new)
       respond_with resource
     end
   end
