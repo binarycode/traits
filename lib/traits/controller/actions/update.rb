6a2e2f77d3ecef19a4a83eb93f5fbb777c6d981f
dieinzige <dieinzige@me.com>
2010-09-30 02:49:14 +0400

    update responder

--- lib/traits/controller/actions/update.rb
+++ lib/traits/controller/actions/update.rb
@@ -2,7 +2,7 @@ module Traits::Controller::Actions::Update
   module InstanceMethods
     def update
       instance_variable_get("@#{singular_name}").update_attributes(params[singular_name])
-      response_with_options instance_variable_get("@#{singular_name}")
+      respond_with instance_variable_get("@#{singular_name}")
     end
   end
   
